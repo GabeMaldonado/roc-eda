61749353bfff7e26c4bca95aba5b88fd0f023ffb
Gabriel Maldonado <us1145@nagases.com>
2020-04-22 15:37:02 -0400

    fixed setup file

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"egmaldonado10@gmail.com\"\n\
+email = \"egmaldonado10@gmail.com.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
-enableCORS=false
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
